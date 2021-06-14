c104ed7504a5bcda80672ddfb952db27b2ccbeb1
Cha Jung Hun <55647934+jhcha08@users.noreply.github.com>
2021-06-14 16:04:16 +0900

    argparse practice

--- argparse_practice.py
+++ argparse_practice.py
@@ -12,5 +12,5 @@ def cylinder_volume(r, h):
 
 print(cylinder_volume(args.radius,args.height))
 
-# 터미널에 C:\Users\User\vscode> 라고 되어있을 때
+# 터미널에 C:\Users\User\(~사용자 작업공간~)> 라고 되어있을 때
 # 그 뒤에 python argparse_practice.py -r 2 -H 4 이런식으로 적으면 됨
