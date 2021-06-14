44ecb7f64ce3bdc7df29cf4d442391817dac2a1f
Cha Jung Hun <55647934+jhcha08@users.noreply.github.com>
2021-06-14 16:03:20 +0900

    argparse practice

---
+++ argparse_practice.py
@@ -0,0 +1,16 @@
+import math
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculate volume of a Cylinder')
+parser.add_argument('-r', '--radius', type=int, help='Radius of Cylinder')
+parser.add_argument('-H', '--height', type=int, help='Height of Cylinder')
+args = parser.parse_args()
+
+def cylinder_volume(r, h):
+    v = (math.pi) * (r**2) * h
+    return v
+
+print(cylinder_volume(args.radius,args.height))
+
+# 터미널에 C:\Users\User\vscode> 라고 되어있을 때
+# 그 뒤에 python argparse_practice.py -r 2 -H 4 이런식으로 적으면 됨
