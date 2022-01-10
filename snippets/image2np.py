7986d2d92e1037b073c579733454ac732310a4cd
Cha Jung Hun <55647934+jhcha08@users.noreply.github.com>
2022-01-10 12:46:38 +0900

    image -> np

--- snippets/image2np.py
+++ snippets/image2np.py
@@ -3,11 +3,11 @@ from PIL import Image
 import numpy as np
 import os
 
-path = '/home/hallasan/junghun/EDSR-PyTorch/dataset/DIV2K/DIV2K_train_LR_bicubic/X4'
+path = 'your path'
 file_list = os.listdir(path)
 i = 0
 
 for file in file_list:
     img = cv2.imread(path+'/'+file)
     result_shrink = np.asarray(Image.open(path+'/'+file))
-    np.save('/home/hallasan/junghun/SR_Training/dataset_div2k/LR/x4/'+'%s.npy' % file[:-4], result_shrink)
+    np.save('your path/'+'%s.npy' % file[:-4], result_shrink)
