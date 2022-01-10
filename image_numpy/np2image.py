d82b0ca877a3bb66bdfc46924020b0a5b0d2f899
Cha Jung Hun <55647934+jhcha08@users.noreply.github.com>
2022-01-10 12:47:28 +0900

    np -> image

--- image_numpy/np2image.py
+++ image_numpy/np2image.py
@@ -2,11 +2,11 @@ import numpy as np
 from PIL import Image
 import os
 
-path = '/home/hallasan/junghun/SR_Training/dataset/mit34_70/LR/x3'
+path = 'your path'
 file_list = os.listdir(path)
 i = 0
 
 for file in file_list:
     n = np.load(path+'/'+file)
     image = Image.fromarray(n)
-    image.save("/home/hallasan/junghun/SR_Training/dataset/mit34_70_img/LRimage/x3/%s.png" % file[:-4])
+    image.save("your path/%s.png" % file[:-4])
