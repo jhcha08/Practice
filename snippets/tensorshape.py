bf443edcf72f8ba549075412992cee68cb7be4a5
Cha Jung Hun <55647934+jhcha08@users.noreply.github.com>
2022-01-10 12:48:37 +0900

    find tensor shape

--- snippets/tensorshape.py
+++ snippets/tensorshape.py
@@ -2,7 +2,7 @@ import numpy as np
 import os 
 from PIL import Image
 
-path = '/home/hallasan/junghun/SR_Training/dataset/MitHuman_add_x4_500/HR'
+path = 'your path'
 file_list = os.listdir(path)
 i = 0
 for file in file_list:
